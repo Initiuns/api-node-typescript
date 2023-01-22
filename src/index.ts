0cdc0c776f58d18dba4e8c9213e3956d80c06231
Initiuns <assisdesouza.felipe55@outlook.com>
2023-01-21 23:04:23 -0300

    feat: resolve production problem

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import { Knex } from './server/database/knex'
 import { server } from './server/Server'
 
-server.listen(process.env.PORT || 5050, () => console.log(`App rodando na porta ${process.env.PORT || 5050}`))
+const startServer = () => {
+  server.listen(process.env.PORT || 5050, () => console.log(`App rodando na porta ${process.env.PORT || 5050}`))
+}
+
+
+if(process.env.ISLOCALHOST !== 'true') {
+  Knex.migrate.latest().then(() => {
+    startServer()
+  })
+    .catch(console.log)
+}
+else {
+  startServer()
+}
